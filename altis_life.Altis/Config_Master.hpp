0fcbcc25900c16fcbe2e74b3bcd5f49eae3c4484
Alexander Petrovic <unitd588@gmail.com>
2015-04-27 17:36:30 -0400

    grilled rabbit + water icon

--- altis_life.Altis/Config_Master.hpp
+++ altis_life.Altis/Config_Master.hpp
@@ -159,7 +159,7 @@ class VirtualItems {
 	//Drink
 	VITEMMACRO(redgull, "STR_Item_RedGull", "redgull", 1, 1500, 200, false, 100, "icons\redgull.paa")
 	VITEMMACRO(coffee, "STR_Item_Coffee", "coffee", 1, 10, 5, false, 100, "icons\coffee.paa")
-	VITEMMACRO(waterBottle, "STR_Item_WaterBottle", "waterBottle", 1, 10, 5, false, 100, "icons\water.paa")
+	VITEMMACRO(waterBottle, "STR_Item_WaterBottle", "waterBottle", 1, 10, 5, false, 100, "icons\waterbottle.paa")
 
 	//Food
 	VITEMMACRO(apple, "STR_Item_Apple", "apple", 1, 65, 50, false, 10, "icons\apple.paa")
