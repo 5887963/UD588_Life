8148a30ad5f6bd6df9403e03b415c06e8b547262
cykapetrovic <unitd588@gmail.com>
2015-05-12 15:40:04 +0100

    Fix DB

--- ud588life.sql
+++ ud588life.sql
@@ -1,30 +1,49 @@
-CREATE DATABASE  IF NOT EXISTS `arma3life` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `arma3life`;
--- MySQL dump 10.13  Distrib 5.6.23, for Win64 (x86_64)
---
--- Host: localhost    Database: arma3life
--- ------------------------------------------------------
--- Server version	5.6.24-log
+-- --------------------------------------------------------
+-- Host:                         lifeserver.ud588.com
+-- Server version:               5.6.24-log - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             9.2.0.4947
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `gangs`
---
+-- Dumping database structure for arma3life
+CREATE DATABASE IF NOT EXISTS `arma3life` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `arma3life`;
 
-DROP TABLE IF EXISTS `gangs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `gangs` (
+
+-- Dumping structure for procedure arma3life.deleteDeadVehicles
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` PROCEDURE `deleteDeadVehicles`()
+BEGIN
+	DELETE FROM `vehicles` WHERE `alive` = 0;
+END//
+DELIMITER ;
+
+
+-- Dumping structure for procedure arma3life.deleteOldGangs
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` PROCEDURE `deleteOldGangs`()
+BEGIN
+  DELETE FROM `gangs` WHERE `active` = 0;
+END//
+DELIMITER ;
+
+
+-- Dumping structure for procedure arma3life.deleteOldHouses
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` PROCEDURE `deleteOldHouses`()
+BEGIN
+  DELETE FROM `houses` WHERE `owned` = 0;
+END//
+DELIMITER ;
+
+
+-- Dumping structure for table arma3life.gangs
+CREATE TABLE IF NOT EXISTS `gangs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `owner` varchar(32) DEFAULT NULL,
   `name` varchar(32) DEFAULT NULL,
@@ -34,17 +53,13 @@ CREATE TABLE `gangs` (
   `active` tinyint(4) DEFAULT '1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name_UNIQUE` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Table structure for table `houses`
---
+-- Data exporting was unselected.
 
-DROP TABLE IF EXISTS `houses`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `houses` (
+
+-- Dumping structure for table arma3life.houses
+CREATE TABLE IF NOT EXISTS `houses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pid` varchar(32) NOT NULL,
   `pos` varchar(64) DEFAULT NULL,
@@ -53,23 +68,17 @@ CREATE TABLE `houses` (
   `owned` tinyint(4) DEFAULT '0',
   PRIMARY KEY (`id`,`pid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `players`
---
+-- Data exporting was unselected.
 
-DROP TABLE IF EXISTS `players`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `players` (
+
+-- Dumping structure for table arma3life.players
+CREATE TABLE IF NOT EXISTS `players` (
   `uid` int(12) NOT NULL AUTO_INCREMENT,
   `name` varchar(32) NOT NULL,
   `playerid` varchar(50) NOT NULL,
   `cash` int(100) NOT NULL DEFAULT '0',
   `bankacc` int(100) NOT NULL DEFAULT '0',
-  `westcash` int(100) NOT NULL DEFAULT '0',
-  `westbankacc` int(100) NOT NULL DEFAULT '0',
   `coplevel` enum('0','1','2','3','4','5','6','7','8','9','10') NOT NULL DEFAULT '0',
   `cop_licenses` text,
   `civ_licenses` text,
@@ -87,17 +96,22 @@ CREATE TABLE `players` (
   UNIQUE KEY `playerid` (`playerid`),
   KEY `name` (`name`),
   KEY `blacklist` (`blacklist`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Table structure for table `vehicles`
---
+-- Data exporting was unselected.
 
-DROP TABLE IF EXISTS `vehicles`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `vehicles` (
+
+-- Dumping structure for procedure arma3life.resetLifeVehicles
+DELIMITER //
+CREATE DEFINER=`arma3`@`localhost` PROCEDURE `resetLifeVehicles`()
+BEGIN
+	UPDATE `vehicles` SET `active`= 0;
+END//
+DELIMITER ;
+
+
+-- Dumping structure for table arma3life.vehicles
+CREATE TABLE IF NOT EXISTS `vehicles` (
   `id` int(12) NOT NULL AUTO_INCREMENT,
   `side` varchar(15) NOT NULL,
   `classname` varchar(32) NOT NULL,
@@ -112,17 +126,13 @@ CREATE TABLE `vehicles` (
   KEY `side` (`side`),
   KEY `pid` (`pid`),
   KEY `type` (`type`)
-) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Table structure for table `wanted`
---
+-- Data exporting was unselected.
 
-DROP TABLE IF EXISTS `wanted`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `wanted` (
+
+-- Dumping structure for table arma3life.wanted
+CREATE TABLE IF NOT EXISTS `wanted` (
   `wantedID` varchar(50) NOT NULL,
   `wantedName` varchar(52) NOT NULL,
   `wantedCrimes` text NOT NULL,
@@ -130,99 +140,8 @@ CREATE TABLE `wanted` (
   `active` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`wantedID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping events for database 'arma3life'
---
-
---
--- Dumping routines for database 'arma3life'
---
-/*!50003 DROP PROCEDURE IF EXISTS `deleteDeadVehicles` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `deleteDeadVehicles`()
-BEGIN
-	DELETE FROM `vehicles` WHERE `alive` = 0;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `deleteOldGangs` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `deleteOldGangs`()
-BEGIN
-  DELETE FROM `gangs` WHERE `active` = 0;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `deleteOldHouses` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `deleteOldHouses`()
-BEGIN
-  DELETE FROM `houses` WHERE `owned` = 0;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `resetLifeVehicles` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
-DELIMITER ;;
-CREATE DEFINER=`arma3`@`localhost` PROCEDURE `resetLifeVehicles`()
-BEGIN
-	UPDATE `vehicles` SET `active`= 0;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2015-05-11 11:15:32
